ee6dc18e169b3707deb42dcbb7b24f9ed75b614d
diljaan6 <140455965+diljaan6@users.noreply.github.com>
2023-07-30 02:30:25 +0500

    pip install telegram bot

---
+++ pip.py
@@ -0,0 +1,8 @@
+import tlelbot
+
+token="6294646462:AAFbFia01foZ9Tk0HV0obP2xciKjIXDILdw6"
+
+bot=telebot.ttelebot(token)
+def.stdef.start(mesage)
+bot.rbot.repy-to(mesage, "welcom to chatbot")
+bot.pobot.polling()
